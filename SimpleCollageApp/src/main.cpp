a2a1d81e9147419f37116cd7c7e6974395ce9a57
Dominik Berner <dominik.berner@gmail.com>
2025-03-21 20:55:31 +0000

    Initial setup

---
+++ SimpleCollageApp/src/main.cpp
@@ -0,0 +1,21 @@
+#include <QtCore/QObject>
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+
+
+int main(int argc, char **argv) {
+
+  QGuiApplication app(argc, argv);
+  app.setApplicationName("SimpleCollage");
+  QQmlApplicationEngine qmlEngine;
+
+ // qmlEngine.load(QUrl("qrc:/main.qml"));
+
+  // HardRock::Game game;
+
+  // qmlEngine.rootObjects().first()->setProperty("game",
+  //                                              QVariant::fromValue(&game));
+
+  return app.exec();
+}
