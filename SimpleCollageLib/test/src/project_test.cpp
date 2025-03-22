570f52ff0c7970985b024ff2c41cf332c133f3f6
Dominik Berner <dominik.berner@gmail.com>
2025-03-22 14:36:20 +0000

    Loading image works

--- SimpleCollageLib/test/src/project_test.cpp
+++ SimpleCollageLib/test/src/project_test.cpp
@@ -9,6 +9,6 @@ TEST_CASE("A new project has an empty image", "[Project]")
 
 TEST_CASE("Adding an image to a project returns the same image", "[Project]") {
   SimpleCollage::Project project;
-  project.addImage(":/images/test.png");
+  project.addImage(":/images/checkerboard_64x64.jpg");
   REQUIRE_FALSE(project.getImage().isNull());
 }
