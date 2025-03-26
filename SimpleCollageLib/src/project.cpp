a735798c0775f810653c197586d4ffaf1b61536a
Dominik Berner <dominik.berner@gmail.com>
2025-03-26 10:28:50 +0000

    Handle nonexsiting image

--- SimpleCollageLib/src/project.cpp
+++ SimpleCollageLib/src/project.cpp
@@ -2,17 +2,15 @@
 
 #include <QImage>
 
-namespace SimpleCollage
-{
-    void Project::addImage(const std::string& path)
-    {
+namespace SimpleCollage {
+void Project::addImage(const std::string &path) {
 
-      QImage image(QString::fromStdString(path));
-      _image = image;
-    }
-
-    const QImage& Project::getImage() const
-    {
-        return _image;
-    }
+  QImage image(QString::fromStdString(path));
+  if (image.isNull()) {
+    throw std::invalid_argument("The image file does not exist.");
+  }
+  _image = image;
 }
+
+const QImage &Project::getImage() const { return _image; }
+} // namespace SimpleCollage
--- SimpleCollageLib/test/src/project_test.cpp
+++ SimpleCollageLib/test/src/project_test.cpp
@@ -1,14 +1,19 @@
-#include <catch2/catch_test_macros.hpp>
 #include <SimpleCollage/project.h>
+#include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("A new project has an empty image", "[Project]")
-{
-    SimpleCollage::Project project;
-    REQUIRE(project.getImage().isNull());
+TEST_CASE("A new project has an empty image", "[Project]") {
+  SimpleCollage::Project project;
+  REQUIRE(project.getImage().isNull());
 }
 
 TEST_CASE("Adding an image to a project returns the same image", "[Project]") {
   SimpleCollage::Project project;
   project.addImage(":/images/checkerboard_64x64.jpg");
   REQUIRE_FALSE(project.getImage().isNull());
+}
+
+TEST_CASE("Passing a nonexisting image to addImage throws an exception",
+          "[Project]") {
+  SimpleCollage::Project project;
+  REQUIRE_THROWS(project.addImage(":/images/nonexisting.jpg"));
 }
