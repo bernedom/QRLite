e69c1bcc8b5a57ec82dd039a71ba124549b8e915
Dominik Berner <dominik.berner@gmail.com>
2025-09-03 08:35:14 +0000

    Reading texts, but not finding codes

--- QRLiteLib/src/scanner.cpp
+++ QRLiteLib/src/scanner.cpp
@@ -1,13 +1,40 @@
 #include <QRLite/scanner.h>
 
+#include <ZXing/BarcodeFormat.h>
+#include <ZXing/ImageView.h>
+#include <ZXing/ReadBarcode.h>
+#include <ZXing/ReaderOptions.h>
+
 namespace QRLite {
 
-    QString Scanner::scan(const QImage& image) {
+QString Scanner::scan(const QImage &image) {
 
-        if(image.isNull()){
+  if (image.isNull()) {
+    return QString();
+  }
 
-        }
-        return "QRLite";
-    }
+  const auto rgbImage = image.convertToFormat(QImage::Format_RGB888);
 
+  if (rgbImage.isNull()) {
+    return QString("Failed to convert image to RGB format");
+  }
+
+  const ZXing::ImageView imageView(rgbImage.bits(), rgbImage.width(),
+                                   rgbImage.height(), ZXing::ImageFormat::RGB);
+
+  ZXing::ReaderOptions decoderOptions;
+  decoderOptions.setTryHarder(true);
+  decoderOptions.setTryRotate(true);
+
+  decoderOptions.setFormats(ZXing::BarcodeFormat::MatrixCodes);
+
+  const auto result = ZXing::ReadBarcodes(imageView, decoderOptions);
+
+  if (result.size() == 0) {
+    return QString("No valid codes found");
+  }
+
+  return QString::fromStdString("QRLite");
 }
+
+} // namespace QRLite
