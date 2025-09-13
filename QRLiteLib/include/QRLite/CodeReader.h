a5f6265c080b5650bd6112b8f8c41274c1be5133
Dominik Berner <dominik.berner@gmail.com>
2025-09-13 13:32:33 +0000

    Add setting videosink

--- QRLiteLib/include/QRLite/CodeReader.h
+++ QRLiteLib/include/QRLite/CodeReader.h
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <QtCore/QObject>
+#include <QtMultimedia/QVideoFrame>
+#include <QtMultimedia/QVideoSink>
+#include <QtMultimedia/qvideosink.h>
+#include <qvariant.h>
+
 namespace QRLite {
-class CodeReader {};
+class CodeReader : public QObject {
+  Q_OBJECT
+
+public:
+  explicit CodeReader(QObject *parent = nullptr) : QObject(parent) {};
+  ~CodeReader() override {};
+
+  void onVideoFrameChanged(const QVideoFrame &frame);
+
+  void setVideoSink(QVideoSink *sink);
+
+  Q_PROPERTY(QVideoSink *videoSink MEMBER _sink WRITE setVideoSink)
+
+private:
+  QVideoSink *_sink = nullptr;
+};
 } // namespace QRLite
--- QRLiteLib/src/CodeReader.cpp
+++ QRLiteLib/src/CodeReader.cpp
@@ -1,1 +1,24 @@
-#include <QRLite/codeReader.h>
+#include <QRLite/CodeReader.h>
+
+#include <QtMultimedia/QVideoFrame>
+#include <QtMultimedia/QVideoSink>
+
+namespace QRLite {
+
+void CodeReader::setVideoSink(QVideoSink *sink) {
+  if (sink == _sink)
+    return;
+
+  if (_sink) {
+    disconnect(_sink, &QVideoSink::videoFrameChanged, this,
+               &CodeReader::onVideoFrameChanged);
+  }
+  _sink = sink;
+  connect(_sink, &QVideoSink::videoFrameChanged, this,
+          &CodeReader::onVideoFrameChanged);
+};
+
+void CodeReader::onVideoFrameChanged(const QVideoFrame &frame) {
+  // Todo implement
+}
+} // namespace QRLite
