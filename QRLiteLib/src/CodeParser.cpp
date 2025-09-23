eb38699cbbdaf3207054521ea624bc7919f27bc7
Dominik Berner <dominik.berner@gmail.com>
2025-09-23 13:03:23 +0000

    Guess URLs

--- QRLiteLib/src/CodeParser.cpp
+++ QRLiteLib/src/CodeParser.cpp
@@ -11,6 +11,16 @@ std::expected<QString, QString> parse(const QString &code) noexcept {
     return "<a href=\"" + code + "\">" + code +
            "</a>"; // Return as a link if it's a URL
   }
+  // If the code looks like a URL without a scheme, prefix with "http://"
+  int lastDot = code.lastIndexOf('.');
+  if (lastDot > 0 && !code.contains(' ')) {
+    QString tld = code.mid(lastDot + 1);
+    // Check TLD length and that it ends with a letter
+    if ((tld.length() == 2 || tld.length() == 3) && tld.back().isLetter() &&
+        code.back().isLetter()) {
+      return "<a href=\"http://" + code + "\">http://" + code + "</a>";
+    }
+  }
   return code; // Return the original code if no conditions are met
 }
 } // namespace QRLite::CodeParser
