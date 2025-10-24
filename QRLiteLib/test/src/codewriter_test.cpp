92c92fa8a39f080d54d8bff26fc71e6f2064c98c
Dominik Berner <dominik.berner@gmail.com>
2025-10-24 09:50:23 +0000

    Storing qrcode to file test

--- QRLiteLib/test/src/codewriter_test.cpp
+++ QRLiteLib/test/src/codewriter_test.cpp
@@ -1,6 +1,31 @@
 #include "QRLite/Scanner.h"
 #include <QRLite/CodeWriter.h>
+#include <QtCore/QTemporaryFile>
 #include <catch2/catch_test_macros.hpp>
+
+namespace {
+
+class TempFile {
+
+public:
+  TempFile() {
+    const auto temp_dir = std::filesystem::temp_directory_path();
+    const auto filename = std::filesystem::path("qrlite_test_tempfile.png");
+    _path = (temp_dir / filename).string();
+  }
+
+  ~TempFile() {
+    std::error_code ec;
+    std::filesystem::remove(_path, ec);
+  }
+
+  const QString fileName() const { return QString::fromStdString(_path); }
+
+private:
+  std::string _path;
+};
+
+} // namespace
 
 TEST_CASE("Writing an empty string returns an empty image", "[CodeWriter]") {
   QRLite::CodeWriter codeWriter;
@@ -29,4 +54,22 @@ TEST_CASE(
 
   REQUIRE(scanResult.has_value() == true);
   REQUIRE(scanResult.value() == originalString);
+}
+
+TEST_CASE("Saving a QR code to a file creates a valid image file",
+          "[CodeWriter]") {
+  QRLite::CodeWriter codeWriter;
+  const QString testString = "File Save Test";
+  TempFile tempFile;
+
+  // Save the QR code to a file
+  codeWriter.saveCodeToFile(testString, tempFile.fileName());
+
+  QFile file(tempFile.fileName());
+  REQUIRE(tempFile.fileName().isEmpty() == false);
+  REQUIRE(file.exists() == true);
+
+  // Load the saved image
+  QImage loadedImage(tempFile.fileName());
+  REQUIRE(loadedImage.isNull() == false);
 }
