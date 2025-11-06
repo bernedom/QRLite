29019ba0e21df3de53bf9e1031223ac32bff6f2b
Dominik Berner <dominik.berner@gmail.com>
2025-11-06 15:05:00 +0000

    Cleanup

---
+++ build-android-bundle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cmake --preset ci-ninja-android-armeabi-v7a-debug
+cmake --preset ci-ninja-android-arm64-v8a-debug
+cmake --preset ci-ninja-android-x86_64-debug
+
+cmake --build ./build_android_armeabi-v7a
+cmake --build ./build_android_arm64-v8a
+cmake --build ./build_android_x86_64
+
+/opt/Qt/6.6.1/android_x86_64/bin/qt-cmake --preset ci-ninja-android-all-archs-debug
